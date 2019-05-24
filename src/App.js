015ac2d3b1926a7f933bc3127f6abe5a21e0d2b4
Yevgeniy <yevgeniyerot@gmail.com>
2019-05-24 11:57:12 -0400

    general grid for the main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React      from 'react';
+import logo       from './logo.svg';
 import './App.css';
+import {Grid}     from "semantic-ui-react";
+import GridColumn from "semantic-ui-react/dist/commonjs/collections/Grid/GridColumn";
+// import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="ui internally celled grid">
+      <div className="row">
+        <div className="three wide column" id="chroom-panel">
+        1</div>
+        <div className="ten wide column" id="msg-panel">
+        2</div>
+        <div className="three wide column" id="users-panel">
+        3</div>
+      </div>
     </div>
-  );
+
+  )
 }
 
 export default App;
